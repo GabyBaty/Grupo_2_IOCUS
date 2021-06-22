d9fd4896ed87798f0d9cd418cd286bcbfc3dab9b
DamianTrajster <74788379+DamianTrajster@users.noreply.github.com>
2021-06-22 18:20:36 -0300

    header subido

--- sprint2/app.js
+++ sprint2/app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
-const port = 3000
-const path = require('path')
-const views = path.join(__dirname,'views')
+const port = 3000;
+const path = require('path');
+const views = path.join(__dirname,'views');
 
-app.use (express.static('public'))
+app.use (express.static('public'));
 app.listen(port, () => console.log('Server UP: Port '+ port));
 
 app.get('/',(req,res) => res.sendFile(path.join(views, 'index.html')));
 app.get('/productDetail',(req,res) => res.sendFile(path.join(views, 'productDetail.html')));
 app.get('/productCart',(req,res) => res.sendFile(path.join(views, 'productCart.html')));
 app.get('/login',(req,res) => res.sendFile(path.join(views, 'login.html')));
+app.get('/layout',(req,res) => res.sendFile(path.join(views, 'layout.html')));
