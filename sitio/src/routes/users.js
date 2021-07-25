410efb38b86f8f343465cfbf769877678cc272d4
maguiamaya <eugenia.amaya@unc.edu.ar>
2021-07-25 17:19:45 -0300

    3er paso de ejs

--- sitio/src/routes/users.js
+++ sitio/src/routes/users.js
@@ -1,9 +1,10 @@
-var express = require('express');
+const express = require('express');
 var router = express.Router();
+const {login} = require('../controllers/usersController')
+
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/login', login)
 
 module.exports = router;
