2f729aad47680c8bf443fe9d87ee1600a00b0f79
maguiamaya <eugenia.amaya@unc.edu.ar>
2021-07-28 18:32:37 -0300

    cambios de index a main

--- sitio/src/app.js
+++ sitio/src/app.js
@@ -4,10 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var mainRouter = require('./routes/main');
 var usersRouter = require('./routes/users');
 var productsRouter = require('./routes/products');
-var contactoRouter = require('./routes/users');
+
 
 
 var app = express();
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('/', indexRouter);
+app.use('/', mainRouter);
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
 
