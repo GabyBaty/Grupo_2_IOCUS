2c40a664715dbae83e376c9681c0cf1389dcbcf7
DamianTrajster <74788379+DamianTrajster@users.noreply.github.com>
2021-08-05 20:11:32 -0300

    index,detalles,filter dinamicos

--- sitio/src/controllers/productsController.js
+++ sitio/src/controllers/productsController.js
@@ -1,6 +1,19 @@
+const productos = require('../data/products_db');
+
+const toThousand = require('../utils/toThousand')
+const finalPrice = require('../utils/finalPrice')
+
 module.exports = {
+    
     detail: (req,res) => {
-        return res.render('products/detail', { title: 'IOCUS-DETALLE' });
+        let producto = productos.find(producto => producto.id === +req.params.id);
+        return res.render('products/detail', { 
+            title: 'IOCUS-DETALLE' ,
+            producto,
+            toThousand,
+            finalPrice
+        
+        });
     },
     cart: (req,res) => {
         return res.render('products/cart', { title: 'IOCUS-CARRITO' });
@@ -13,6 +26,11 @@ module.exports = {
         return res.render('products/edit', { title: 'Editar prodcuto' });
     },
     filter: (req,res) => {
-        return res.render('products/filter', { title: 'IOCUS-LISTA' });
+        return res.render('products/filter', { 
+            title: 'IOCUS-LISTA',
+            productos,
+            toThousand,
+            finalPrice
+         });
     },
 }
--- sitio/src/routes/products.js
+++ sitio/src/routes/products.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const {detail, cart, add, edit, filter} = require('../controllers/productsController');
 
 /* GET products page. */
-router.get('/detail', detail);
-router.get('/cart', cart);
+router.get('/detail/:id', detail);
+router.get('/cart/:id', cart);
 router.get('/add', add);
 router.get('/edit', edit);
 router.get('/filter',filter);
