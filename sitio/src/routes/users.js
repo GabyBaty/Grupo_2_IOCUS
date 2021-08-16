4941a816f4351e56592ae348c26e03881feaabd5
DamianTrajster <74788379+DamianTrajster@users.noreply.github.com>
2021-08-16 17:51:23 -0300

    Formulario de registro, validaciones y estructura

--- sitio/src/routes/users.js
+++ sitio/src/routes/users.js
@@ -1,9 +1,12 @@
 const express = require('express');
 var router = express.Router();
-const {login} = require('../controllers/usersController')
+const {login,processRegister} = require('../controllers/usersController')
+
+const registerValidations= require('../validations/registerValidation')
 
 
 /* GET users listing. */
 router.get('/login', login);
+router.post('/login',registerValidations, processRegister);
 
 module.exports = router;
