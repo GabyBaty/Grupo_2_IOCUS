34930fdcda65b89fb9fd3f865b0ba5eaeac6088a
Fefeojeda <ferj.ojeda@gmail.com>
2021-08-16 16:59:58 -0300

    user profile view

--- sitio/src/controllers/usersController.js
+++ sitio/src/controllers/usersController.js
@@ -1,6 +1,10 @@
 module.exports = {
     login: (req,res) => {
         return res.render('users/login', { title: 'IOCUS-LOGIN' });
+    },
+    profile: (req,res) => {
+        return res.render('users/profile', {
+            title: 'Mi perfil'
+        })
     }
-  
 }
--- sitio/src/routes/users.js
+++ sitio/src/routes/users.js
@@ -1,9 +1,9 @@
 const express = require('express');
 var router = express.Router();
-const {login} = require('../controllers/usersController')
+const {login,profile} = require('../controllers/usersController')
 
 
 /* GET users listing. */
 router.get('/login', login);
-
+router.get('/profile', profile);
 module.exports = router;
