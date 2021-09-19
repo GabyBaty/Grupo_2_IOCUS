d9333c07cbc61c20b212f8790ddd9a6bd3309ed0
DamianTrajster <74788379+DamianTrajster@users.noreply.github.com>
2021-09-19 15:26:36 -0300

    sequelize

---
+++ sitio/database/config/config.js
@@ -0,0 +1,24 @@
+module.exports={
+  "development": {
+    username: "root",
+    password: "root",
+    database: "iocus_db",
+    host: "127.0.0.1",
+    dialect: "mysql",
+ 
+  },
+  "test": {
+    username: "root",
+    password: "root",
+    database: "iocus_db",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  "production": {
+    username: "root",
+    password:"root",
+    database: "databaiocus_db",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  }
+}
